3c787daaaaf79adf0a0b142e99970483deeef3a6
die-coding <sugeng.sulistiyawan@student.uns.ac.id>
2018-09-23 21:18:41 +0700

    add default $options

--- ToastrBase.php
+++ ToastrBase.php
@@ -44,7 +44,23 @@ class ToastrBase extends Widget
      *
      * @var array $options
      */
-    public $options = [];
+    public $options = [
+        "closeButton"       => false,
+        "debug"             => false,
+        "newestOnTop"       => true,
+        "progressBar"       => true,
+        "positionClass"     => "toast-top-right",
+        "preventDuplicates" => false,
+        "onclick"           => null,
+        "showDuration"      => "300",
+        "hideDuration"      => "1000",
+        "timeOut"           => "5000",
+        "extendedTimeOut"   => "1000",
+        "showEasing"        => "swing",
+        "hideEasing"        => "linear",
+        "showMethod"        => "fadeIn",
+        "hideMethod"        => "fadeOut",
+    ];
 
     const TYPE_INFO    = 'info';
     const TYPE_ERROR   = 'error';
