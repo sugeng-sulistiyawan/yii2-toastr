762ea155c04efb67b578994e85e2bb03028fd5c4
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2018-10-21 23:52:31 +0700

    add phpunit.xml.dist

---
+++ tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the diecoding/yii2-toastr project.
+ * (c) Die Coding! <https://www.diecoding.com/>
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */ 
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+Yii::setAlias('@tests', __DIR__);
+
+new \yii\console\Application([
+    'id' => 'testApp',
+    'basePath' => __DIR__
+]);
