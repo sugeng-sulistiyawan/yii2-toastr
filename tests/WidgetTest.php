9cd7a52f6336d78d97bf3e3c7f565f20049d9218
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2023-03-29 21:06:51 +0700

    prepare release v2.0.0

---
+++ tests/WidgetTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace diecoding\yii2\toastr\tests;
+
+/**
+ * Class WidgetTest
+ */
+class WidgetTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * 
+     */
+    public function testView1_Controller1()
+    {
+        $this->assertSame('test', YII_ENV);
+    }
+}
